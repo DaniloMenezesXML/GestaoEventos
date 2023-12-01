8be29dce65e74faf41b49255e4912a81ddbf5888
Danilo Silva de Menezees <142947959+DaniloMenezesXML@users.noreply.github.com>
2023-12-01 19:51:34 -0300

    Add files via upload

---
+++ TrabalhoEventos/view/participante.py
@@ -0,0 +1,99 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'Participante.ui'
+##
+## Created by: Qt User Interface Compiler version 6.2.4
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QHeaderView, QLabel, QLineEdit,
+    QMainWindow, QPushButton, QSizePolicy, QTabWidget,
+    QTableWidget, QTableWidgetItem, QWidget)
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        if not MainWindow.objectName():
+            MainWindow.setObjectName(u"MainWindow")
+        MainWindow.resize(790, 597)
+        self.centralwidget = QWidget(MainWindow)
+        self.centralwidget.setObjectName(u"centralwidget")
+        self.widget = QWidget(self.centralwidget)
+        self.widget.setObjectName(u"widget")
+        self.widget.setGeometry(QRect(7, 7, 771, 581))
+        self.tabWidget = QTabWidget(self.widget)
+        self.tabWidget.setObjectName(u"tabWidget")
+        self.tabWidget.setGeometry(QRect(0, 0, 771, 581))
+        self.tab = QWidget()
+        self.tab.setObjectName(u"tab")
+        self.pushButton_2 = QPushButton(self.tab)
+        self.pushButton_2.setObjectName(u"pushButton_2")
+        self.pushButton_2.setGeometry(QRect(307, 200, 151, 51))
+        self.pushButton_3 = QPushButton(self.tab)
+        self.pushButton_3.setObjectName(u"pushButton_3")
+        self.pushButton_3.setGeometry(QRect(307, 130, 151, 51))
+        self.lineEdit = QLineEdit(self.tab)
+        self.lineEdit.setObjectName(u"lineEdit")
+        self.lineEdit.setGeometry(QRect(7, 272, 751, 271))
+        self.tabWidget.addTab(self.tab, "")
+        self.tab_2 = QWidget()
+        self.tab_2.setObjectName(u"tab_2")
+        self.lineEdit_2 = QLineEdit(self.tab_2)
+        self.lineEdit_2.setObjectName(u"lineEdit_2")
+        self.lineEdit_2.setGeometry(QRect(10, 40, 581, 41))
+        self.label = QLabel(self.tab_2)
+        self.label.setObjectName(u"label")
+        self.label.setGeometry(QRect(10, 0, 171, 51))
+        self.pushButton = QPushButton(self.tab_2)
+        self.pushButton.setObjectName(u"pushButton")
+        self.pushButton.setGeometry(QRect(620, 40, 121, 41))
+        self.tb_lista_sessao_2 = QTableWidget(self.tab_2)
+        if (self.tb_lista_sessao_2.columnCount() < 4):
+            self.tb_lista_sessao_2.setColumnCount(4)
+        __qtablewidgetitem = QTableWidgetItem()
+        self.tb_lista_sessao_2.setHorizontalHeaderItem(0, __qtablewidgetitem)
+        __qtablewidgetitem1 = QTableWidgetItem()
+        self.tb_lista_sessao_2.setHorizontalHeaderItem(1, __qtablewidgetitem1)
+        __qtablewidgetitem2 = QTableWidgetItem()
+        self.tb_lista_sessao_2.setHorizontalHeaderItem(2, __qtablewidgetitem2)
+        __qtablewidgetitem3 = QTableWidgetItem()
+        self.tb_lista_sessao_2.setHorizontalHeaderItem(3, __qtablewidgetitem3)
+        self.tb_lista_sessao_2.setObjectName(u"tb_lista_sessao_2")
+        self.tb_lista_sessao_2.setGeometry(QRect(10, 100, 741, 441))
+        self.tabWidget.addTab(self.tab_2, "")
+        MainWindow.setCentralWidget(self.centralwidget)
+
+        self.retranslateUi(MainWindow)
+
+        self.tabWidget.setCurrentIndex(0)
+
+
+        QMetaObject.connectSlotsByName(MainWindow)
+    # setupUi
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
+        self.pushButton_2.setText(QCoreApplication.translate("MainWindow", u"Desinscrever-me", None))
+        self.pushButton_3.setText(QCoreApplication.translate("MainWindow", u"Inscrever-me", None))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab), QCoreApplication.translate("MainWindow", u"Inicio", None))
+        self.label.setText(QCoreApplication.translate("MainWindow", u"<html><head/><body><p><span style=\" font-size:11pt;\">E-mail Participante</span></p></body></html>", None))
+        self.pushButton.setText(QCoreApplication.translate("MainWindow", u"Consultar", None))
+        ___qtablewidgetitem = self.tb_lista_sessao_2.horizontalHeaderItem(0)
+        ___qtablewidgetitem.setText(QCoreApplication.translate("MainWindow", u"Nome do Participante", None));
+        ___qtablewidgetitem1 = self.tb_lista_sessao_2.horizontalHeaderItem(1)
+        ___qtablewidgetitem1.setText(QCoreApplication.translate("MainWindow", u"Evento", None));
+        ___qtablewidgetitem2 = self.tb_lista_sessao_2.horizontalHeaderItem(2)
+        ___qtablewidgetitem2.setText(QCoreApplication.translate("MainWindow", u"Sess\u00e3o", None));
+        ___qtablewidgetitem3 = self.tb_lista_sessao_2.horizontalHeaderItem(3)
+        ___qtablewidgetitem3.setText(QCoreApplication.translate("MainWindow", u"Hor\u00e1rio da Sessao", None));
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_2), QCoreApplication.translate("MainWindow", u"Agenda", None))
+    # retranslateUi
+
