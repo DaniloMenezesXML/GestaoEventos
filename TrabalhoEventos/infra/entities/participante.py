1c1e548bc40a7c3e6e71da60a416d01f790b3a41
HenriquePrudencio <141193621+HenriquePrudencio@users.noreply.github.com>
2023-12-01 20:30:38 -0300

    Add files via upload

--- TrabalhoEventos/infra/entities/participante.py
+++ TrabalhoEventos/infra/entities/participante.py
@@ -8,8 +8,10 @@ class Participante(Base):
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     nome: Mapped[str] = mapped_column(nullable=False)
     email: Mapped[str] = mapped_column(nullable=False)
+    lista_eventos_inscritos: Mapped[list] = mapped_column(nullabe=False)
     ativo: Mapped[bool] = mapped_column(default=True, nullable=False)
     sessao = relationship("Sessão", back_populates="participante", cascade="save-update")
 
     def __repr__(self):
-        return f'Participante [nome= {self.nome}, E-mail= {self.email}]'
+        return (f'Participante [nome= {self.nome}, E-mail= {self.email},'
+                f' Eventos Inscritos{self.lista_eventos_inscritoss}]')
