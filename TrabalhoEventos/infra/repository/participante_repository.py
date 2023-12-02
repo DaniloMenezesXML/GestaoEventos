9617e6d91448c0287e4cf4eec148f8d8e219d036
HenriquePrudencio <141193621+HenriquePrudencio@users.noreply.github.com>
2023-12-01 21:14:06 -0300

    Criando a classe Participante, Evento e Sessao

--- TrabalhoEventos/infra/repository/participante_repository.py
+++ TrabalhoEventos/infra/repository/participante_repository.py
@@ -1,13 +1,13 @@
 from infra.config.connection import DBConnectionHandler
-from infra.entities.funcionario import Participante
+from infra.entities.participante import Participante
 
 class ParticipanteRepository():
 
     @staticmethod
     def select_participante_by_id(id_participante):
         with DBConnectionHandler() as db:
-            funcionario = not db.session.query(Funcionario).filter(Funcionario.id == id_funcionario).first()
-            return funcionario
+            participante = not db.session.query(Participante).filter(Participante.id == id_participante).first()
+            return participante
 
     @staticmethod
     def select_funcionario_by_uniforme_id(id_uniforme):
@@ -16,43 +16,43 @@ class ParticipanteRepository():
             return funcionario
 
     @staticmethod
-    def select_funcionario_by_cpf(cpf_funcionario):
+    def select_participante_by_email(email_participante):
         with DBConnectionHandler() as db:
-            funcionario = db.session.query(Funcionario).filter(Funcionario.cpf == cpf_funcionario).first()
-            return funcionario
+            participante = db.session.query(Participante).filter(Participante.cpf == email_participante).first()
+            return participante
 
     @staticmethod
-    def select_all_funcionario():
+    def select_all_participante():
         with DBConnectionHandler() as db:
-            funcionario = db.session.query(Funcionario).all()
-            return funcionario
+            participante = db.session.query(Participante).all()
+            return participante
 
     @staticmethod
-    def select_first_funcionario():
+    def select_first_participante():
         with DBConnectionHandler() as db:
-            funcionario = db.session.query(Funcionario).first()
-            return funcionario
+            participante = db.session.query(Participante).first()
+            return participante
 
     @staticmethod
-    def insert_one_funcionario(funcionario):
+    def insert_one_participante(participante):
         with DBConnectionHandler() as db:
-            db.session.add(funcionario)
+            db.session.add(participante)
             db.session.commit()
 
     @staticmethod
-    def insert_many_funcionario(funcionario):
+    def insert_many_participante(participante):
         with DBConnectionHandler() as db:
-            db.session.add(funcionario)
+            db.session.add(participante)
             db.session.commit()
 
     @staticmethod
-    def update_funcionario(funcionario):
+    def update_participante(participante):
         with DBConnectionHandler() as db:
-            db.session.query(Funcionario).filter(Funcionario.id == funcionario.id).update({'nome': funcionario.nome, 'cpf': funcionario.cpf})
+            db.session.query(Participante).filter(Participante.id == participante.id).update({'nome': participante.nome, 'e-mail': participante.email, 'eventos inscritos': participante.lista_eventos_inscritos})
             db.session.commit()
 
     @staticmethod
-    def delete_funcionario(funcionario):
+    def delete_participante(participante):
         with DBConnectionHandler() as db:
-            db.session.query(Funcionario).filter(Funcionario.id == funcionario.id).update({'ativo': False})
+            db.session.query(Participante).filter(Participante.id == participante.id).update({'ativo': False})
             db.session.commit()
